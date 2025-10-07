0b05c53852d328df9917ca6f24c5208592550fad
Haroon K M <haroonint144@gmail.com>
2025-10-07 20:19:37 +0530

    inbuilt modelfile

--- model.py
+++ model.py
@@ -1,22 +1,15 @@
-import ollama 
-
-model_description = input("Enter a brief description of the model: ")
-modelfile=f"""
-FROM deepseek-r1:latest
-SYSTEM {model_description}
-PARAMETER temperature 0.6
-PARAMETER top_p 0.9
-MAX_TOKENS 1024
-"""
-
-assistant_name = input("Enter the name of the assistant: ")
 import ollama
 
+model_description = input("Enter a brief description of the model: ")
+assistant_name = input("Enter a name for the model: ")
 ollama.create(
     model=assistant_name,
-    template=modelfile
+    from_="deepseek-r1:latest",
+    system=model_description,
+    parameters={"temperature": 0.2, "top_p": 0.5},
+    stream=False
 )
 
 msg_input = input("Ask Anything ")
 res = ollama.chat(model=assistant_name,messages=[{'role':'user','content':msg_input}])
-print(res['content'])
+print(res['messege']['content'])
